6666a8821c0263a4b472d51be487f435e325a468
Vitaly <motor.kodlife@gmail.com>
2015-11-06 21:12:13 -0500

    chore(reports): Let at-labourcost-report directive display table data

--- src/js/apps/platform/reports/at-labourcost-report/at-labourcost-report.directive.js
+++ src/js/apps/platform/reports/at-labourcost-report/at-labourcost-report.directive.js
@@ -5,11 +5,37 @@
     .module('alcPlatform.reports')
     .directive('atLabourcostReport', atLabourcostReport);
 
-  function atLabourcostReport() {
+  atLabourcostReport.$inject = ['$rootScope', 'restalchemy', 'navigation'];
+  function atLabourcostReport($rootScope, $restalchemy, $navigation) {
     return {
+      replace: true,
       scope: {},
-      templateUrl: 'src/js/apps/platform/reports/at-labourcost-report/at-labourcost-report.html'
-      //template: 'at-labourcost-report'
+      templateUrl: 'src/js/apps/platform/reports/at-labourcost-report/at-labourcost-report.html',
+      controller: atLabourcostReportCtrl,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    function atLabourcostReportCtrl() {
+      var vm = this;
+
+      $navigation.select({
+        forward: "reports",
+        selected: "labourreport"
+      });
+
+      // Initialise the REST api
+      var rest = $restalchemy.init({ root: $rootScope.config.api.labourstats.root });
+      rest.api = $rootScope.config.api.labourstats;
+
+      rest.at(rest.api.costs).get().then(function(costdata) {
+        vm.costdata = _.union(costdata[0].directContractors, costdata[0].providers);
+        vm.sortBy('name');
+      });
+      
+      vm.sortBy = function(fieldName) {
+          vm.costdata = _.sortBy(vm.costdata , fieldName);
+      }
     }
   }
 
