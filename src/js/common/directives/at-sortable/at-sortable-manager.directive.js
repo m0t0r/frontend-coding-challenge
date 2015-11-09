411a123aa7346a60748151dbff108a538bcb8897
Vitaly <motor.kodlife@gmail.com>
2015-11-08 19:21:43 -0500

    chore(directives): Add support for default sorting and row freezing in at-sortable-manager directive

--- src/js/common/directives/at-sortable/at-sortable-manager.directive.js
+++ src/js/common/directives/at-sortable/at-sortable-manager.directive.js
@@ -10,13 +10,30 @@
         scope: {
           sortData: '='
         },
-        controller: atSortableManagerCtrl,
-        controllerAs: 'manage',
-        bindToController: true
+        controller: atSortableManagerCtrl
       };
 
-      function atSortableManagerCtrl() {
-        var manage = this, previousColumn;
+      atSortableManagerCtrl.$inject = ['$scope'];
+
+      function atSortableManagerCtrl($scope) {
+        var ctrl = this, previousColumn, columns = [], unbindWatcher;
+
+        // once sort data is loaded check on default column and sort it
+        unbindWatcher = $scope.$watch('sortData', function(isSortDataLoaded) {
+          if(isSortDataLoaded) {
+            _.each(columns, function(column) {
+              if(!!column.isolateScope().default) {
+                ctrl.sortBy(column);
+              }
+            });
+            columns.length = 0;
+            unbindWatcher();
+          }
+        });
+
+        this.registerColumn = function(column) {
+          columns.push(column);
+        };
 
         this.sortBy = function(column) {
           if(previousColumn === column) {
@@ -29,17 +46,44 @@
             }
           }
 
-          function toggleSortOrder(column) {
-            if(column.isolateScope().order === 'asc') {
-              manage.sortData = _.sortBy(manage.sortData, column.isolateScope().column).reverse();
-              column.isolateScope().order = 'desc';
-            } else {
-              manage.sortData = _.sortBy(manage.sortData, column.isolateScope().column);
-              column.isolateScope().order = 'asc';
-            }
-          }
           column.isolateScope().toggleChevronClass();
           previousColumn = column;
+        };
+
+        function toggleSortOrder(column) {
+          if(column.isolateScope().order === 'asc') {
+            $scope.sortData = sort(column).reverse();
+            column.isolateScope().order = 'desc';
+          } else {
+            $scope.sortData = sort(column);
+            column.isolateScope().order = 'asc';
+          }
+
+          if(column.isolateScope().freezeValue) {
+            freezeRow(column)
+          }
+        }
+
+        function sort(column) {
+          return _.sortBy($scope.sortData, function(row) {
+            if (typeof row[column.isolateScope().column] === 'string') {
+              return row[column.isolateScope().column].toLowerCase();
+            } else {
+              return row[column.isolateScope().column];
+            }
+          });
+        }
+
+        function freezeRow(column) {
+          var index, row;
+
+          index  = _.findIndex($scope.sortData, function(row) {
+            return row[column.isolateScope().column] == column.isolateScope().freezeValue;
+          });
+
+          row = $scope.sortData[index];
+          $scope.sortData.splice(index, 1);
+          $scope.sortData.unshift(row);
         }
       }
     }
